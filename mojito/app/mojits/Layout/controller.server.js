756a1bc1eb1782b8f2bada2a54d6a7c3c1c1d3d5
imalberto <imalberto@gmail.com>
2012-07-24 19:18:03 -0700

    NEW: add support for FirstByteFrameMojit
    Signed-off-by: imalberto <imalberto@gmail.com>

---
+++ mojito/app/mojits/Layout/controller.server.js
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2012 Yahoo! Inc. All rights reserved.
+ */
+/*jslint anon:true, sloppy:true, nomen:true*/
+YUI.add('Layout', function(Y, NAME) {
+
+/**
+ * The Layout module.
+ *
+ * @module Layout
+ */
+
+    /**
+     * Constructor for the Controller class.
+     *
+     * @class Controller
+     * @constructor
+     */
+    Y.mojito.controllers[NAME] = {
+
+        init: function(config) {
+            this.config = config;
+        },
+
+        /**
+         * Method corresponding to the 'index' action.
+         *
+         * @param ac {Object} The ActionContext that provides access
+         *        to the Mojito API.
+         */
+        index: function(ac) {
+            ac.firstbyte.done();
+        }
+
+    };
+
+}, '0.0.1', {requires: ['mojito', 'mojito-firstbyte-addon']});
